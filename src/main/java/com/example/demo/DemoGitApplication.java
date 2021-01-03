eb55b41285549ee54785aa5be2c79fec1d7afb45
Dipendra Hamal <40882015+Dipen404@users.noreply.github.com>
2021-01-03 17:04:44 +0545

    second commit from github

--- src/main/java/com/example/demo/DemoGitApplication.java
+++ src/main/java/com/example/demo/DemoGitApplication.java
@@ -10,7 +10,7 @@ public class DemoGitApplication {
 	
 	@GetMapping("/home")
 	public String showMessage() {
-		return "Welcome to spring boot";
+		return "Welcome to spring boot. This is just for demo purpose";
 	}
 
 	public static void main(String[] args) {
